559209388282902fd83d29b053be51668cce3d5d
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2021-10-31 02:05:07 +0900

    add resolve typescript 'paths' on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
   extends: ['@cybozu/eslint-config/presets/node-typescript-prettier'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  rules: {
+    'import/no-unresolved': 'error',
+    'node/no-missing-import': 'off',
+  },
 };
