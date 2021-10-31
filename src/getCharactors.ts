9e329737c6261e74168fcd05becabd0ee0108a43
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2021-10-31 16:21:32 +0900

    add exporting csv file

--- src/getCharactors.ts
+++ src/getCharactors.ts
@@ -1,6 +1,10 @@
 import puppeteer from 'puppeteer';
+import { writeFile } from 'fs/promises';
+import { resolve } from 'path';
+import csvStringifySync from 'csv-stringify/lib/sync';
 
 const LIST_URL = 'https://gamewith.jp/sangokushi-shinsen/article/show/263306';
+const DIST_FILEPATH = resolve('dist', 'charactorURLs.csv');
 
 const device = puppeteer.devices['Pixel 4'];
 const gotoOptions: puppeteer.WaitForOptions = {
@@ -14,13 +18,23 @@ const gotoOptions: puppeteer.WaitForOptions = {
   await page.emulate(device);
 
   await page.goto(LIST_URL, gotoOptions);
-  const count = await page.evaluate(() => {
+  const charactorNames = await page.evaluate(() => {
     return Array.from(
       document.querySelectorAll!<HTMLAnchorElement>(
         '.s_shinsen_ichiran table tr.w-idb-element td:first-child a'
       )
-    ).map((elm) => ({ name: elm.innerText, url: elm.href }));
+    ).map((elm) => ({
+      gwId: elm.href.split('/').reverse()[0],
+      name: elm.innerText,
+      url: elm.href,
+    }));
   });
-  console.log(count);
+  // console.log(charactorNames);
+
+  const csvData = csvStringifySync(charactorNames, {
+    header: true,
+    quoted_string: true,
+  });
+  await writeFile(DIST_FILEPATH, csvData);
   await browser.close();
 })();
