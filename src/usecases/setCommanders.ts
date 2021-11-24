bf7e436b4f9fdb45a391d3c3ad33d4820a251698
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2021-11-24 13:32:12 +0900

    add sample implementation

--- src/usecases/setCommanders.ts
+++ src/usecases/setCommanders.ts
@@ -1,6 +1,18 @@
+import { GSSCommander } from '~/drivers/spreadsheets/datamodel';
 import { build } from '~/drivers/spreadsheets/factory';
 
 (async () => {
   const { doc, sheets } = await build();
-  console.log(doc, sheets);
+
+  const { commander: sheet } = sheets;
+  if (sheet !== undefined) {
+    const headers = GSSCommander.getProperties();
+    await sheet.resize({
+      rowCount: sheet.rowCount,
+      columnCount: headers.length,
+    });
+    await sheet.setHeaderRow(headers);
+    const commanders = await sheet.getRows();
+    console.log(headers, commanders);
+  }
 })();
