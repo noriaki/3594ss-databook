a4b00e370fc9b138c477c945109a8f478dd70b65
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2021-11-27 16:00:00 +0900

    implements getting ALL Commanders

--- src/usecases/syncCommandersWebToSpreadSheets.ts
+++ src/usecases/syncCommandersWebToSpreadSheets.ts
@@ -17,23 +17,23 @@ import { GSSCommander } from '~/drivers/spreadsheets/datamodel';
   const { browser, page } = await buildCrawler();
   const commanders = await getCommanderList(page);
 
-  const sampleCommanders = commanders.filter(
-    (c) =>
-      c.gwId === '296471' ||
-      c.gwId === '289306' ||
-      c.gwId === '266994' ||
-      c.gwId === '266931'
-  );
+  // const sampleCommanders = commanders.filter(
+  //   (c) =>
+  //     c.gwId === '296471' ||
+  //     c.gwId === '289306' ||
+  //     c.gwId === '266994' ||
+  //     c.gwId === '266931'
+  // );
 
   const { doc, sheets } = await buildSpreadSheets();
   const { commander: sheet } = sheets;
   if (sheet !== undefined) {
     await syncHeader(sheet);
 
-    for (const commander of sampleCommanders) {
+    // for (const commander of sampleCommanders) {
+    for (const commander of commanders) {
       const data = await getCommanderDetail(page, commander);
-      const c = new Commander(data.asCommanderObject());
-      const ssData = new GSSCommander(c);
+      const ssData = new GSSCommander(new Commander(data.asCommanderObject()));
       const ssRow = await findCommanderById(sheet, ssData.id);
       if (ssRow !== undefined) {
         await compareAndUpdateCommander(sheet, ssRow, ssData);
